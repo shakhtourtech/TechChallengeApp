365ae67fc65ab9dbbdcf201f2dc077497e7c2112
Suleiman Shakhtour <sushakht@microsoft.com>
2022-10-20 09:36:04 +0300

    output the dns of public ip

--- solution/terraform/outputs.tf
+++ solution/terraform/outputs.tf
@@ -1,10 +1,10 @@
-##################################################################
+###############################################################################
 #
-# Application can be accessed by browsing output of this public ip
+# Application can be accessed by browsing output of this public ip dns name
 #
-##################################################################
-output "public_ip" {
-  value       = azurerm_public_ip.base.ip_address
+###############################################################################
+output "public_ip_dns_name" {
+  value       = azurerm_public_ip.base.*.fqdn
   description = "A public ip address where application can be browsed"
 }
 
